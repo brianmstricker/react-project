e1ae333eac8ca0243146129c4fa7172496f8cc45
Brian Stricker <brianmstricker@gmail.com>
2023-02-22 02:05:11 -0500

    homepage images/responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,50 +1,68 @@
-import { Navbar, NavbarBrand, Collapse, NavbarToggler, Nav, NavItem } from "reactstrap";
-import React from 'react'
-import Logo from '../../assets/logo.png'
-import './Header.css'
-import styled from 'styled-components'
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  NavbarToggler,
+  Nav,
+  NavItem,
+} from "reactstrap";
+import React from "react";
+import Logo from "../../assets/logo.png";
+import "./Header.css";
+import styled from "styled-components";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 const NavbarCustom = styled(Navbar)`
-flex-direction: column;`
-const NavbarBrandcustom = styled(NavbarBrand)`
-  `
+  flex-direction: column;
+`;
+const NavbarBrandcustom = styled(NavbarBrand)``;
 const ImgLogo = styled.img`
   width: 40px;
   margin-right: 1rem;
-`
+`;
 const HeaderH1 = styled.h1`
   font-size: 2.75rem;
   font-weight: 500;
-  `
+`;
 const NavItemcustom = styled(NavItem)`
-margin-left: 1rem;
-`
+  margin-left: 1rem;
+  display: inline-block;
+  &:hover,
+  &:active {
+    color: #ff0000;
+    text-decoration: underline;
+    text-underline-offset: 0.75rem;
+  }
+`;
 const ParaDiv = styled.div`
   margin-top: -1rem;
   margin-left: 8rem;
-`
+`;
 
 const Header = () => {
   return (
     <div>
-       <NavbarCustom light expand="md">
-                <NavbarBrandcustom href="/" className="ms-1">
-                    <ImgLogo className="float-start" src={Logo} alt="Logo" />
-                    <HeaderH1>Pizza Delicioso</HeaderH1>
-                </NavbarBrandcustom>
-                <Nav className="m-2" navbar>
-                  <NavItemcustom className="list-unstyled">About</NavItemcustom>
-                  <NavItemcustom className="list-unstyled">Register</NavItemcustom>
-                  <NavItemcustom className="list-unstyled">Login</NavItemcustom>
-                  <NavItemcustom className="list-unstyled"><ShoppingCartOutlinedIcon/></NavItemcustom>
-                </Nav>
-       </NavbarCustom>
-       <ParaDiv className="d-none d-md-block">
+      <NavbarCustom light expand="md">
+        <NavbarBrandcustom href="/" className="ms-1">
+          <ImgLogo className="float-start" src={Logo} alt="Logo" />
+          <HeaderH1>Pizza Delicioso</HeaderH1>
+        </NavbarBrandcustom>
+        <Nav className="m-2" navbar>
+          <NavItemcustom className="list-unstyled">Order</NavItemcustom>
+          <NavItemcustom className="list-unstyled">Menu</NavItemcustom>
+          <NavItemcustom className="list-unstyled">About</NavItemcustom>
+          <NavItemcustom className="list-unstyled">Register</NavItemcustom>
+          <NavItemcustom className="list-unstyled">Login</NavItemcustom>
+          <NavItemcustom className="list-unstyled">
+            <ShoppingCartOutlinedIcon />
+          </NavItemcustom>
+        </Nav>
+      </NavbarCustom>
+      <ParaDiv className="d-none d-md-block">
         Your favorite local pizzeria
-       </ParaDiv>
+      </ParaDiv>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import burger from "../../assets/burger.jpg";
+import pasta from "../../assets/pasta.jpg";
 
+const MainDiv = styled.div`
+  height: 80vh;
+  display: flex;
+`;
+const OpenDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  margin-left: 5rem;
+  margin-top: 3rem;
+`;
+const BurgerImg = styled.img`
+  width: 60%;
+  height: 40%;
+  margin: 20px;
+  margin-left: 7rem;
+  object-fit: cover;
+  object-position: right;
+  overflow: auto;
+  float: none;
+  @media (max-width: 1100px) {
+    transition: 1s ease;
+    object-position: 75% 20px;
+  }
+  @media (min-width: 1001px) {
+    transition: 1s ease;
+  }
+  @media (max-width: 900px) {
+    margin: 0;
+  }
+`;
+const CardDiv = styled.div`
+  display: flex;
+  align-items: stretch;
+  margin-left: 15rem;
+  @media (max-width: 1400px) {
+    margin-left: auto;
+  }
+`;
+const StyledP = styled.p`
+  font-size: 1.25rem;
+`;
 const Main = () => {
   return (
-    <div>Main</div>
-  )
-}
+    <MainDiv>
+      <OpenDiv>
+        <h2 className="text-decoration-underline text-uppercase">
+          We're Open 7 Days a Week!
+        </h2>
+        <StyledP>
+          Mon ~ Thurs: 10:00 AM ~ 10:00 PM <br />
+          Fri & Sat: 10:00 AM ~ 10:00 PM <br />
+          Sun: 10:00 PM ~ 10:00PM
+        </StyledP>
+      </OpenDiv>
+      <CardDiv>
+        <BurgerImg src={burger} alt="burger"></BurgerImg>
+        <BurgerImg src={pasta} alt="burger"></BurgerImg>
+      </CardDiv>
+    </MainDiv>
+  );
+};
 
-export default Main
+export default Main;
